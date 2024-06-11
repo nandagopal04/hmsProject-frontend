4dbea4edd14e65067076ea01c186d1e65eb0c789
Ikkurthi Nanda Gopal <nanduikkurthi@gmail.com>
2024-06-11 16:21:47 +0530

    first commit

---
+++ src/Doctor.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Doctor = () => {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    //get
+    const fetchDoctors = async () => {
+      try {
+        const response = await axios.get('http://hospitalmanagementsystem-env.eba-prkbihnf.ap-southeast-2.elasticbeanstalk.com/doctor');
+        setDoctors(response.data);
+      } catch (error) {
+        console.error('Error fetching doctors:', error);
+      }
+    };
+
+
+    fetchDoctors();
+  }, []);
+
+  return (
+    <div>
+      <center>
+        <h2>Doctors</h2>
+        {
+          doctors.map(doctor => (
+            <div key={doctor.id}>
+              <p><strong>{doctor.name}</strong> - {doctor.speciality}</p>
+              <p>Doctor ID: {doctor.id}</p>
+            </div>
+          ))
+        }
+
+      </center>
+    </div>
+  );
+};
+
+export default Doctor;
